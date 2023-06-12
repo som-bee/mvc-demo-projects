8f519e78489338f50265610a27ff10747f7c1a71
Somnath Bera <somnathbera1919@gmail.com>
2023-06-12 11:32:07 +0530

    Project 5 added

---
+++ MVCApp-IdentityFramework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCApp_IdentityFramework.Startup))]
+namespace MVCApp_IdentityFramework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
